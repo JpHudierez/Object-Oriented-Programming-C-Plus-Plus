faa3116dc36c8523d61d342427239269845c49c1
JpHudierez <jphudierez4@gmail.com>
2025-08-27 23:00:17 -0700

    Lab1: add Hello.cpp & screenshots

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,14 @@
+/**
+ * Name: Jp Hudierez
+ * Description: A simple C++ program that prints "Hello, World!" and "Hello World again!" to the console.
+ **/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
